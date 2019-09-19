265c909ad058b954b80cb8e8f497087c1c719247
YutoShibata07 <yuto19990715@gmail.com>
2019-09-19 14:00:46 +0900

    円グラフにアニメーションをつけた。達成率を表示するアニメーションをviewWillAppearに設定した。completedLableのAutolayoutがうまくいかない。。。

--- Achievement/Controller/GraphViewController.swift
+++ Achievement/Controller/GraphViewController.swift
@@ -10,33 +10,37 @@ import UIKit
 
 class GraphViewController: UIViewController {
     let shapeLayer = CAShapeLayer()
+    let pulsatingLayer = CAShapeLayer()
     let sharedUserData:UserData = UserData.sharedData
     var achieveRate:Double = 0
-    var middleRate:Double = 0
     var NumberOfTask:Int!
     var timer:Timer?
     let percentageLbl:UILabel = {
         let label = UILabel()
-        label.text = "Start"
+        label.text = ""
         label.textAlignment = .center
         label.font = UIFont.boldSystemFont(ofSize: 32)
         label.textColor = UIColor.black
         return label
     }()
+    let completedLbl:UILabel = {
+       let label = UILabel()
+        label.text = "completed"
+        label.textAlignment = .center
+        label.font =  UIFont.systemFont(ofSize: CGFloat(10))
+        label.textColor = UIColor.black
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(percentageLbl)
-        percentageLbl.frame = CGRect(x: 0, y: 0, width: 130, height: 100)
-        percentageLbl.center = view.center
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        middleRate = 0.0
-        makePieChart()
         percentageLbl.alpha = 0
+        completedLbl.alpha = 0
+        makePieChart()
     }
-    
-    func makePieChart(){//達成率を表示する関数。
+    private func makePieChart(){//達成率を表示する関数。
         sharedUserData.data.doneCount = 0
         NumberOfTask = routines.count
         for task  in routines {
@@ -50,60 +54,66 @@ class GraphViewController: UIViewController {
         }else{
             achieveRate = 0
         }
-        print(achieveRate)
-        let trackLayer = CAShapeLayer()
+        //アニメーション用の円を作る。
+        createCircleLayer(layer: pulsatingLayer, strokeColor: .clear, fillColor: .init(hex: "EEFFDE"), lineWidth: 10, strokeEnd: 1)
+        animatePulsatingLayer()
         //デフォルトの枠を作る。
+        let trackLayer = CAShapeLayer()
+        createCircleLayer(layer: trackLayer, strokeColor: .lightGray, fillColor: .white, lineWidth: 20, strokeEnd: 1)
+        createCircleLayer(layer: shapeLayer, strokeColor: .init(hex: "5EC220"), fillColor: .clear, lineWidth: 20, strokeEnd: 0)
+        view.addSubview(percentageLbl)
+        percentageLbl.frame = CGRect(x: 0, y: 0, width: 130, height: 100)
+        percentageLbl.center = view.center
+        view.addSubview(completedLbl)
+        completedLbl.topAnchor.constraint(equalTo: self.percentageLbl.bottomAnchor, constant: 5).isActive = true
+        completedLbl.leadingAnchor.constraint(equalTo: percentageLbl.leadingAnchor).isActive = true
+//        completedLbl.widthAnchor.constraint(equalTo: percentageLbl.widthAnchor).isActive = true
+//        completedLbl.topAnchor.constraint(equalTo: percentageLbl.bottomAnchor, constant: 20).isActive = true
+        shapeLayer.strokeEnd = 0
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
+        shapeLayer.strokeEnd = 0
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        shapeLayer.strokeEnd = 0
+        startMoveChart()
+    }
+    private func createCircleLayer(layer:CAShapeLayer, strokeColor:UIColor, fillColor:UIColor, lineWidth:Int,strokeEnd:Int){
         let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle:
             2 * CGFloat.pi , clockwise: true)
-        trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.lightGray.cgColor
-        trackLayer.fillColor = UIColor.clear.cgColor
-        trackLayer.position = view.center
-        trackLayer.lineWidth = 10
-        trackLayer.lineCap = CAShapeLayerLineCap.round
-        trackLayer.strokeEnd = 1
-        view.layer.addSublayer(trackLayer)
-        //        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
-        shapeLayer.path = circularPath.cgPath
-        shapeLayer.strokeColor = UIColor.green.cgColor
-        shapeLayer.lineWidth = 10
-        shapeLayer.lineCap = CAShapeLayerLineCap.round
-        shapeLayer.strokeEnd = 0
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
-        shapeLayer.position = view.center
-        //        shapeLayer.strokeEnd = CGFloat(middleRate)
-        print(middleRate)
-        view.layer.addSublayer(shapeLayer)
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.fillColor = fillColor.cgColor
+        layer.position = view.center
+        layer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
+        layer.lineWidth = CGFloat(lineWidth)
+        layer.lineCap = CAShapeLayerLineCap.round
+        layer.strokeEnd = CGFloat(strokeEnd)
+        view.layer.addSublayer(layer)
     }
-    @objc func handleTap(){
+    func startMoveChart(){
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        basicAnimation.duration = 1.5
+        basicAnimation.duration = 1.3
         shapeLayer.strokeEnd = CGFloat(achieveRate)
         percentageLbl.text = "\(Int(achieveRate * 100))%"
         UIView.animate(withDuration: 0.3, delay: 1.5, options: [.curveEaseIn], animations: {
+            self.completedLbl.alpha = 1
             self.percentageLbl.alpha = 1
         },completion: nil)
 //        basicAnimation.fillMode = CAMediaTimingFillMode.forwards//アニメーションが残り続けるようにする。
 //        basicAnimation.isRemovedOnCompletion = false//何回もアニメーションを行えるようにする。
         shapeLayer.add(basicAnimation, forKey: "BasicAnimation")
         return
-        
     }
-//    func getRate(){//shapeLayer.strokeEndにアニメーションを持たせるためにmiddleRateの値を変化させる。
-//        var timer:Timer?
-//        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(addRate), userInfo: nil, repeats: true)
-//
-//    }
-//    @objc func addRate(){
-//        while middleRate <= achieveRate {
-//            self.middleRate += 0.1
-//        }
-//        print("\(middleRate)だよ")
-//        print("Stop!")
-//        timer?.invalidate()
-//    }
-    
-    
+    private func animatePulsatingLayer(){
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        animation.toValue = 1.3
+        animation.duration = 0.8
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        pulsatingLayer.add(animation, forKey: "pulsing")
+    }
 }
